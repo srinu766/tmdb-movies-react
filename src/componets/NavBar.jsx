ef97f873978ab60eaef7597e820af60512468669
srinu <sopparisrinu637@gmail.com>
2024-04-07 13:51:36 +0530

    updated

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom'
 const NavBar = () => {
   return (
     <div className='flex border space-x-8  items-center pl-3 py-3 shadow-sm'>
-    <img className='w-[50px]' src={Logo} alt='' />
+
+    <img className='w-[50px]' src={Logo} alt='image' />
     <Link to='/' className='text-blue-500 font-bold text-2xl'>Home</Link>
     <Link to='/watchlist' className='text-blue-500 font-bold text-2xl'>Watch List</Link>
     </div>
